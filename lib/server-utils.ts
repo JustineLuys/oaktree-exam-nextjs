178d272e33713b6e4c9bb1459e5f6c848efa95d4
Justine Paul <justineluyon9@gmail.com>
2025-04-04 01:27:51 +0800

    Deployment failed
    - Added localhost url to env

--- lib/server-utils.ts
+++ lib/server-utils.ts
@@ -2,10 +2,11 @@ import 'server-only'
 import { apiResponseItemSchema, apiResponseItemsSchema } from './schema'
 import { ErrorApiResponse, Item } from './types'
 
+const API_BASE_URL = process.env.API_BASE_URL
 export const fetchItem = async (
   id: number
 ): Promise<ErrorApiResponse | Item> => {
-  const res = await fetch(`http://localhost:8000/api/items/${id}`, {
+  const res = await fetch(`${API_BASE_URL}/api/items/${id}`, {
     headers: {
       Authorization:
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqdXN0aW5lenhjMSIsImlkIjoyLCJleHAiOjE3NDYyNzYwMjJ9.Afi_fjRsAskgAVwSkHPnMmonXQjN57dkv8HrudRKocQ',
@@ -26,7 +27,7 @@ export const fetchItem = async (
 }
 
 export const fetchItems = async (): Promise<ErrorApiResponse | Item[]> => {
-  const res = await fetch('http://localhost:8000/api/items', {
+  const res = await fetch(`${API_BASE_URL}/api/items`, {
     headers: {
       Authorization:
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqdXN0aW5lenhjMSIsImlkIjoyLCJleHAiOjE3NDYyNzYwMjJ9.Afi_fjRsAskgAVwSkHPnMmonXQjN57dkv8HrudRKocQ',
