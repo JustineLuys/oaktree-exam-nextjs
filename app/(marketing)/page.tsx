f1a74e8263089d84509a37cdc7b6de71415cea34
Justine Paul <justineluyon9@gmail.com>
2025-04-05 08:00:19 +0800

    Added force-dynamic
    Added force-dynamic

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,6 +2,8 @@ import AddItemDialog from '@/components/dialog/AddItemDialog'
 import ItemsList from '@/components/list/ItemsList'
 import PageItemsListSkeleton from '@/components/PageItemsListSkeleton'
 import { Suspense } from 'react'
+export const dynamic = 'force-dynamic'
+
 const HomePage = () => {
   return (
     <main className="h-full xl:w-[85%] w-full border pt-5 flex flex-col gap-10 items-center justify-around">
